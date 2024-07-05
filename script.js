d9e702f5a0f4d3d6b0e460c2c99312eec56a0f6a
Dev.Huang <18778418865@163.com>
2024-07-05 18:02:11 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+function updateLengthValue(value) {
+    document.getElementById('lengthValue').innerText = value;
+}
+
+function generatePassword() {
+    const length = document.getElementById('lengthRange').value;
+    const includeLetters = document.getElementById('includeLetters').checked;
+    const includeNumbers = document.getElementById('includeNumbers').checked;
+    const includeSymbols = document.getElementById('includeSymbols').checked;
+
+    let characters = '';
+    const letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const numbers = '0123456789';
+    const symbols = ';,:/_@*#\ˇ-';
+
+    if (includeLetters) characters += letters;
+    if (includeNumbers) characters += numbers;
+    if (includeSymbols) characters += symbols;
+
+    if (characters === '') {
+        alert('Please select at least one character type');
+        return;
+    }
+
+    let password = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        password += characters[randomIndex];
+    }
+
+    document.getElementById('passwordOutput').value = password;
+}
